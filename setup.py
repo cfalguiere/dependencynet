6e38be0640e27c6c940f1126e4c8868be67acdfe
Claude Falguiere <cfalguiere@gmail.com>
2021-01-23 19:52:38 +0100

    v0.0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as file:
 setup(
     name='dependencynet',
     packages=find_packages(include=['dependencynet']),
-    version='0.0.3',
+    version='0.0.4',
     description='represent and analyse dependency graphs (networks)',
     long_description=long_description,
     long_description_content_type='text/markdown',
