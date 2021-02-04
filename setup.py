05b5e060d505f2911b37fb1f29a344e4c3f523f2
Claude Falguiere <cfalguiere@gmail.com>
2021-02-04 22:57:30 +0100

    graph tests

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as file:
 setup(
     name='dependencynet',
     packages=find_packages(include=['dependencynet']),
-    version='0.0.4',
+    version='0.0.5',
     description='represent and analyse dependency graphs (networks)',
     long_description=long_description,
     long_description_content_type='text/markdown',
