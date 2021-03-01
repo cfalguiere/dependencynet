bbc76245a1eb3e39d562d0326a9d4f5e2671f08a
Claude Falguiere <cfalguiere@gmail.com>
2021-03-01 13:05:57 +0100

    v0.1.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
                                     'dependencynet.network',
                                     'dependencynet.datasource',
                                     'dependencynet.datasource.core']),
-    version='0.1.5',
+    version='0.1.6',
     description='represent and analyse dependency graphs (networks)',
     long_description=long_description,
     long_description_content_type='text/markdown',
