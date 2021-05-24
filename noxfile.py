e052e6b6817dbf78fd89d32414a82b141975c0d8
Claude Falguiere <cfalguiere@gmail.com>
2021-05-24 16:33:48 +0200

    lint and check notebook + moved notebook fanout + comments in notebook

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 nox.options.sessions = ['lint', 'tests']
-
+nox.options.reuse_existing_virtualenvs = True
 
 @nox.session
 def tests(session):
@@ -37,3 +37,43 @@ def tests_fanout(session):
     session.install('--quiet', '-r', 'requirements.txt')
     session.install('-e', '.')
     session.run('pytest', '-m', 'fanout')
+
+
+@nox.session
+def tests_notebooks(session):
+    session.install('pytest', 'testfixtures', 'coverage', 'pytest-cov', 'pytest-notebook')
+    session.install('--quiet', '-r', 'requirements.txt')
+    session.install('-e', '.')
+    session.run('pytest', '--nb-test-files')
+
+
+@nox.session
+def lint_notebooks(session):
+    # flakehell requieres toml
+    # session.install('flake8', 'flakehell')
+    # session.run(
+    #        'flakehell',
+    #        'lint',
+    #        './notebooks/')
+    session.install('nblint')
+    session.install('--quiet', '-r', 'requirements.txt')
+    session.run(
+            'nblint',
+            '--linter',
+            'pyflakes',
+            './notebooks/scenario/fanout/example-graphml-fanout.ipynb')
+
+
+@nox.session
+def check_notebooks(session):
+    session.install('nbconvert')
+    session.install('--quiet', '-r', 'requirements.txt')
+    session.run(
+            'jupyter',
+            'nbconvert',
+            '--to',
+            'notebook',
+            '--execute',
+            '--output',
+            'output',
+            './notebooks/scenario/fanout/example-graphml-fanout.ipynb')
